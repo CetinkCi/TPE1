89507771a860c438b3a108dcb54a5f7788490243
CetinkCi <Cigdem_Cetinkaya@hotmail.de>
2016-03-23 23:23:50 +0100

    Klasse Waehrungen

--- TPE1/src/Waehrung.java
+++ TPE1/src/Waehrung.java
@@ -38,6 +38,40 @@ public class Waehrung {
         
         return info;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((kuerzel == null) ? 0 : kuerzel.hashCode());
+        result = prime * result + (int) (kurs ^ (kurs >>> 32));
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Waehrung other = (Waehrung) obj;
+        if (kuerzel == null) {
+            if (other.kuerzel != null)
+                return false;
+        } else if (!kuerzel.equals(other.kuerzel))
+            return false;
+        if (kurs != other.kurs)
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        return true;
+    }
     
     
 }
---
+++ TPE1/src/Waehrungen.java
@@ -0,0 +1,15 @@
+
+public class Waehrungen {
+
+    
+    Waehrung Dollar = new Waehrung("US Dollar","USD",100L);
+    
+    Waehrung Rubel = new Waehrung("Rubel","RUB",255L);
+    
+    Waehrung Yen = new Waehrung("Yen","Yen",91L);
+    
+    Waehrung Franken = new Waehrung("Schweizer Franken","CHF",10509L);
+    
+    Waehrung Euro = new Waehrung("Euro","EU",12690L);
+    
+}
